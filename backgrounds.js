c52459be6dda801c40a18529709e5f1b1840e053
austin-knauer <austinknauer@alumni.colostate.edu>
2020-09-29 22:50:30 -0600

    Update backgrounds.js

--- backgrounds.js
+++ backgrounds.js
@@ -1,62 +1,62 @@
 const backgrounds = [
     {
-        url: 'url("/woman.jpg") 0 0 / cover no-repeat',
+        url: 'url(/woman.jpg) 0 0 / cover no-repeat',
         panel: '#5b7031',
         button: '#0b0c05',
         font: '#BA55D3',
         border: 'white'
     },
     {
-        url: 'url("/tents.jpg") 0 0 / cover no-repeat',
+        url: 'url(/tents.jpg) 0 0 / cover no-repeat',
         panel: '#b6761e',
         button: '#8a763e',
         font: 'white',
         border: 'white'
     },
     {
-        url: 'url("/cliff.jpg") 0 0 / cover no-repeat',
+        url: 'url(/cliff.jpg) 0 0 / cover no-repeat',
         panel: '#caa364',
         button: '#1e0c0c',
         font: 'cyan',
         border: 'white'
     },
     {
-        url: 'url("/trees.jpg") 0 0 / cover no-repeat',
+        url: 'url(/trees.jpg) 0 0 / cover no-repeat',
         panel: '#2d301b',
         button: '#795d1c',
         font: 'white',
         border: 'white'
     },
     {
-        url: 'url("/hills.jpg") 0 0 / cover no-repeat',
+        url: 'url(/hills.jpg) 0 0 / cover no-repeat',
         panel: '#d9dab6',
         button: '#76878b',
         font: 'red',
         border: 'white'
     },
     {
-        url: 'url("/sunset.jpg") 0 0 / cover no-repeat',
+        url: 'url(/sunset.jpg) 0 0 / cover no-repeat',
         panel: '#932609',
         button: '#153330',
         font: 'white',
         border: 'white'
     },
     {
-        url: 'url("/forest.jpg") 0 0 / cover no-repeat',
+        url: 'url(/forest.jpg) 0 0 / cover no-repeat',
         panel: '#40541C',
         button: '#81840d',
         font: 'white',
         border: 'white'
     },
     {
-        url: 'url("/purple.jpg") 0 0 / cover no-repeat',
+        url: 'url(/purple.jpg) 0 0 / cover no-repeat',
         panel: '#6e2328',
         button: '#181919',
         font: 'white',
         border: 'white'
     },
     {
-        url: 'url("/cloud.jpg") 0 0 / cover no-repeat',
+        url: 'url(/cloud.jpg) 0 0 / cover no-repeat',
         panel: '#595e5a',
         button: '#937038',
         font: '#551011',
